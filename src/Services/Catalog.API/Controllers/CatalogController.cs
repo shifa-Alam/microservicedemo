d8ffae060f7da39d828ebd7fa42d89670b457717
Shifa Alam <shifa7446@gmail.com>
2023-04-09 20:50:00 +0600

    ResposeCase Added

--- src/Services/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -18,11 +18,20 @@ namespace Catalog.API.Controllers
             _productManager = productManager;
         }
         [HttpGet]
-        [ProducesResponseType(typeof(IEnumerable<Product>),(int) HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 30)]
         public IActionResult GetProducts()
         {
-            var products = _productManager.GetAll();
-            return CustomResult(products);
+            try
+            {
+                var products = _productManager.GetAll();
+                return CustomResult(products);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+
         }
     }
 }
